45021ad6f63453ee78f83ba01f2731e27748a697
Hytan Jara <hjara@estudiantec.cr>
2023-02-22 11:20:45 -0600

    Merge resuelto

--- quiz.cpp
+++ quiz.cpp
@@ -3,9 +3,7 @@
 using namespace std;
 
 int main(){
-<<<<<<< HEAD
-	cout<< " este es el código que hizo pique"<<endl;
-=======
+
 	cout<< " este es el código que hizo shakira"<<endl;
->>>>>>> shakira
+
 }
